32f4ba5550f63a96d8bdb4c759d4062e9839b919
virginie-marceillant <virginie.marceillant@gmail.com>
2022-11-03 10:10:52 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<form action="traitement.php" method="GET">
+
+<select name="genre">
+
+ <option value= "HOMME">HOMME</option>
+ <option value= "FEMME">FEMME</option>
+ <option value= "INCONNU">INCONNU</option>
+</select> <br>
+
+Nom: <input type = "text" name="nom"><br>
+Prénom:<input type= "text" name= "prenom"><br>
+Code Postal:<input type= "text" name="Code Postal"><br>
+Ville:<input type= "text" name="Ville"><br>
+
+Paiement: CB <input type="radio" name="CB" value="CB">
+
+Paypal: <input type="radio" name="Paypal" value="Paypal"><br>
+
+Conditions d'utilisation : <input type = "checkbox" name="CU" value= "CU"><br>
+
+<input type="submit" name="OK" value ="OK">
+
+
+
+
